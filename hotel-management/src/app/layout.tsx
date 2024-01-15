4efe62914353f03491c9370b9eff099d7db50b42
ANKIT1149 <140632363+ANKIT1149@users.noreply.github.com>
2024-01-15 18:44:01 +0530

    Header Completed footer in progress

--- hotel-management/src/app/layout.tsx
+++ hotel-management/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 const poppins = Poppins({ subsets: ['latin'],  weight: ["400", '500', '700', "900"], style: ["italic", 'normal'], variable: "--font-Poppins"})
 
@@ -17,7 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
-         <main>{children}</main>
+         <main>
+          <Header />
+          {children}
+          <Footer />
+          </main>
       </body>
     </html>
   )
